9c4f1682a730b822630112f8275aa3dc238e2e9f
nstraightbeam <yz6926@nyu.edu>
2025-10-05 14:57:19 -0400

    Add GET /about route to backend API

--- back-end/app.js
+++ back-end/app.js
@@ -78,5 +78,40 @@ app.post('/messages/save', async (req, res) => {
   }
 })
 
+// a route to handle fetching about us content
+app.get('/about', async (req, res) => {
+  try {
+    const aboutData = {
+      name: "Cyryl Zhang",
+      location: "New York, NY",
+      education: "Junior studying Game Design and Computer Science at NYU",
+      bio: [
+        "Hi! I'm Cyryl Zhang from Vancouver, Canada. Currently, I'm in my junior year at NYU, where I'm pursuing a dual focus in Game Design and Computer Science.",
+        "Recently, I've been deeply engaged with games like Hades 2 and Papers Please. Hades 2's incredible art style and roguelike mechanics have been inspiring my own game design philosophy, while Papers Please's unique approach to storytelling through bureaucratic gameplay has shown me how powerful minimalist design can be.",
+        "When I'm not coding or designing games, you'll find me exploring new indie games, travelling with my family, or exploring the latest gelato place.",
+        "This MERN stack project is part of my Agile Software Development and DevOps class, where I'm learning to build full-stack applications using modern web technologies."
+      ],
+      interests: [
+        "Game Design",
+        "Matcha",
+        "Indie Games",
+        "Traveling",
+        "Some movies and all the animes"
+      ],
+      currentlyPlaying: ["Hades 2", "Papers Please"],
+      imageUrl: "/images/cyryl-photo.jpg",
+      status: "all good"
+    }
+    
+    res.json(aboutData)
+  } catch (err) {
+    console.error(err)
+    res.status(500).json({
+      error: err.message,
+      status: 'failed to retrieve about data'
+    })
+  }
+})
+
 // export the express app we created to make it available to other modules
 module.exports = app // CommonJS export style!
